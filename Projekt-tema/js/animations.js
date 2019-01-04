cc25ab0f481a1e4e2959d953688cf997d39d1959
Søren Nørup <soeren96@hotmail.com>
2019-01-04 12:55:58 +0100

    get the topposition of the elements after the subheader divs
    The subheader animation will be triggered when the scrolltop reaches the topposition of the next element.

--- Projekt-tema/js/animations.js
+++ Projekt-tema/js/animations.js
@@ -33,27 +33,36 @@
   
     });
   slide_in_text = $(function(){
-
+    // get the topposition of the elements after the subheader divs
+    var row_top = top_off_main_row_class();
+    alert(row_top);
+    
     $(window).load(function(){
-        
+      
        $('.left-widget-text').addClass('onload');
        $('.right-widget-text').addClass('onload');
        
         });
    $(window).scroll(function(){
-       if($(this).scrollTop() < 200){
+        
+       if($(this).scrollTop() < row_top){
+          
           $('.left-widget-text').addClass('onload');
           $('.right-widget-text').addClass('onload');
        }
-       else if($(this).scrollTop()>300){
+       else if($(this).scrollTop()>row_top){
           $('.left-widget-text').removeClass('onload');
           $('.right-widget-text').removeClass('onload');
        }
-      
-      
-      
+
       });
-    
-    
+
     });
+  // get the topposition of the elements after the subheader divs
+  function top_off_main_row_class(){
+   //var pos = $('.main-row').offset();
+   var pos = document.getElementsByClassName('main-row');
+   
+   return pos[0].offsetTop;
+  }
    
